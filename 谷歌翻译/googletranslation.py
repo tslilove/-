3296c34dd36fd4f1312de5ab55c8fed7cf86f3c9
tslilove <92263055+tslilove@users.noreply.github.com>
2022-02-09 15:50:54 +0800

    Update googletranslation.py

--- 谷歌翻译/googletranslation.py
+++ 谷歌翻译/googletranslation.py
@@ -1,12 +1,3 @@
-#!/usr/bin/python
-# -*- coding:utf-8 -*-
-# @Time    :2021/12/29 10:45
-# @Author   : 亮哥
-# @Mail    : yxl20160@126.com
-# @File      : googletranslation.py
-# Software  : PyCharm
-
-
 import requests,re
 from language_setting import setting
 
