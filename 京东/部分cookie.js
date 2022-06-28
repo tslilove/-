a2a8c0b7db2d16c12f858298d720da42cf2d42c6
tslilove <92263055+tslilove@users.noreply.github.com>
2022-06-28 21:56:51 +0800

    Create 部分cookie.js

---
+++ 京东/部分cookie.js
@@ -0,0 +1,36 @@
+
+function _jda() {
+    return "122270672.1648950889038758030994.1648950889.1648950889.1648950889.1"
+}
+function _jdb() {
+    return "122270672.1.1648950889038758030994|1.1648950889"
+}
+function _jdc() {
+    return "122270672"
+}
+function _jdv() {
+    return "122270672|direct|-|none|-|"+(new Date).getTime()
+}
+function shshshfpb() {
+    return "usa4w8nrVxHVxHwMxDdtSyA"
+}
+function shshshfp() {
+    return '94d128b3-9e9c-3f34-c5f2-591018ad5306-1642586801'
+}
+function shshshfpa() {
+    return '94d128b3-9e9c-3f34-c5f2-591018ad5306-1642586801'
+}
+function shshshsID() {
+    return "bd90b69af8a2a2d672846f55117950d4_1_1648953381472"
+}
+function _3AB9D23F7A4B3C9B() {
+    return "K5BMO6QSWSXCVAA5IXB5YX3T42GTB6ALNGN2HPV3NISFXPEXJXWPUI5PFAN2TY4JNIRHT6VIUQ6S2KZ46PB5GSMTRI"
+}
+
+function cookeie(){
+    var clickid=0,
+        rkv=1.0,
+        qrsc=1;
+
+}
+console.log(_jdv());
